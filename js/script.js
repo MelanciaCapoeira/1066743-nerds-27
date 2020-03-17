1f17479598f259ea790239fa20164f2f842e17c3
Anatoly <ananiksim@gmail.com>
2020-03-18 01:13:35 +0300

    подключение карты

---
+++ js/script.js
@@ -0,0 +1,79 @@
+var link = document.querySelector(".btn_map");
+var popup = document.querySelector(".write_us");
+var close = popup.querySelector(".close_cross");
+var form = popup.querySelector("form")
+var email = popup.querySelector("[name=e-mail]");
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal_show");
+});
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal_show");
+    popup.classList.remove("modal_error");
+});
+
+form.addEventListener("submit", function (evt) {
+    if (!email.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal_error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal_error");
+    }
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal_show")) {
+        popup.classList.remove("modal_show");
+        popup.classList.remove("modal_error");
+      }
+    }
+});
+
+var link_map = document.querySelector(".img_map");
+var link_marker = document.querySelector(".map_marker");
+var popup_map = document.querySelector(".popup_map");
+var close_map = popup_map.querySelector(".close_cross");
+
+link_map.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup_map.classList.add("modal_show_map");
+});
+
+link_marker.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup_map.classList.add("modal_show_map");
+});
+
+close_map.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup_map.classList.remove("modal_show_map");
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup_map.classList.contains("modal_show_map")) {
+        popup_map.classList.remove("modal_show_map");
+      }
+    }
+});
+
+ymaps.ready(init);
+    function init(){
+        // Создание карты.
+        var myMap = new ymaps.Map("map", {
+            // Координаты центра карты.
+            // Порядок по умолчанию: «широта, долгота».
+            // Чтобы не определять координаты центра карты вручную,
+            // воспользуйтесь инструментом Определение координат.
+            center: [55.76, 37.64],
+            // Уровень масштабирования. Допустимые значения:
+            // от 0 (весь мир) до 19.
+            zoom: 7
+        });
+    }
